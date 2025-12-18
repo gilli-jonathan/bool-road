13243e59228b12a62d5b55c3240eafa7a5614ac0
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-12-18 14:29:35 +0100

    add card

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,36 +1,30 @@
 import data from "../assets/data"
 import { useState } from "react"
-import { Link } from "react-router-dom";
+import Card from "../components/Card";
 
 export default function Homepage() {
 
     const [viaggi, setViaggi] = useState(data)
     console.log(viaggi);
 
-
+    // const = `/${journey.id}`
 
 
 
     return (
 
-        <section className="homepage">
+        <section className=" homepage">
             <div className="container">
-                <div className="row row-cols-2 row-cols-lg-3">
+
+
+                <div className="row row-cols row-cols-lg-2">
                     {viaggi.map((journey => (
 
-                        <div className="col" key={journey.id} >
-                            <Link to={`/${journey.id}`} >
-
-                                <div className="card" >
-                                    <h2>{journey.meta}</h2>
-                                    <h2>{journey.data_partenza}</h2>
-                                    <h2>{journey.data_ritorno}</h2>
-                                </div>
-                            </Link>
+                        <div className="col p-4">
+                            <Card key={journey.id} image={journey.immagine} title={journey.meta} inizio={journey.data_partenza} fine={journey.data_ritorno} link={`/${journey.id}`} />
                         </div>
-                    )))
 
-                    }
+                    )))}
                 </div>
 
             </div>
